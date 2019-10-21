e8a3dfd0c15a09412a6dbeffd3c6f790b6712c0f
Lauren Albert <pleaseonthekeys@gmail.com>
2019-10-21 17:33:40 -0400

    server requires morgan and body-parser

--- server/index.js
+++ server/index.js
@@ -1,8 +1,11 @@
 const express = require('express');
+const morgan = require('morgan');
+const Parser = require('body-parser');
 const app = express();
 const port = 3000;
 
 // app.get('/', (req, res) => res.send('Hello World!'));
 app.use(express.static('dist'));
+app.use(morgan('tiny'));
 
 app.listen(port, () => console.log(`Example app listening on port ${port}!`));
