4ad406771e8b0673be2ccc36fb854f389141eff0
Lauren Albert <pleaseonthekeys@gmail.com>
2019-10-21 19:28:59 -0400

    server-setup uses body-parser with .json

--- server/index.js
+++ server/index.js
@@ -7,5 +7,6 @@ const port = 3000;
 // app.get('/', (req, res) => res.send('Hello World!'));
 app.use(express.static('dist'));
 app.use(morgan('tiny'));
+app.use(Parser.json());
 
 app.listen(port, () => console.log(`Example app listening on port ${port}!`));
