142038aad775d8ff4b23350f2c206ab4cd77fe94
Brittany Yee <brittanyyee1@gmail.com>
2019-10-21 16:10:01 -0400

    Set up file structure and webpack

---
+++ client/app.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+
+class App extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {};
+  }
+
+  render() {
+    return <div>Hello {name}</div>;
+  }
+}
+
+export default App;
