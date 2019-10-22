4fad1279cf724b6a7750a9ff6ac23d1f12258d5c
Lauren Albert <pleaseonthekeys@gmail.com>
2019-10-22 19:00:56 -0400

    merging master

--- client/components/overview-components/Overview.jsx
+++ client/components/overview-components/Overview.jsx
@@ -2,9 +2,7 @@ import React, { Component } from 'react';
 import { makeStyles } from '@material-ui/core/styles';
 import Paper from '@material-ui/core/Paper';
 import Grid from '@material-ui/core/Grid';
-//import materialUI grid
-//each imported component will be a grid item
-//Overview is a grid container
+
 import ImgGallery from './ImgGallery';
 import ProductInfo from './ProductInfo';
 import StyleSelector from './StyleSelector';
