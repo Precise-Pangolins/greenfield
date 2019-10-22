fb8a6739ef1a9916ac59a709a60e251735077d58
richard <richard.horvath001@gmail.com>
2019-10-22 17:14:04 -0400

    initial redux setup: file structure, store, example reducers and Provider for app

--- client/index.js
+++ client/index.js
@@ -1,7 +1,14 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import App from './app.jsx';
-import '../src/styles.css';
+import React from "react";
+import ReactDOM from "react-dom";
+import App from "./app.jsx";
+import "../src/styles.css";
+import { Provider } from "react-redux";
+import store from "../src/redux/store/index.js";
 
-var mountNode = document.getElementById('app');
-ReactDOM.render(<App />, mountNode);
+var mountNode = document.getElementById("app");
+ReactDOM.render(
+  <Provider store={store}>
+    <App />
+  </Provider>,
+  mountNode
+);
