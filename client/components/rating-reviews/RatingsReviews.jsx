f125f25909ba401a0526b73a31cfe747a338d98f
richard <richard.horvath001@gmail.com>
2019-10-23 11:42:30 -0400

    initialized frame for ratings and reviews

--- client/components/rating-reviews/RatingsReviews.jsx
+++ client/components/rating-reviews/RatingsReviews.jsx
@@ -1,7 +1,34 @@
 import React from "react";
-
+import { makeStyles } from "@material-ui/core/styles";
+import Paper from "@material-ui/core/Paper";
+import Grid from "@material-ui/core/Grid";
+const useStyles = makeStyles(theme => ({
+  root: {
+    flexGrow: 1
+  },
+  paper: {
+    padding: theme.spacing(2),
+    textAlign: "center",
+    color: theme.palette.text.secondary
+  }
+}));
 const RatingsReviews = () => {
-  return <div></div>;
+  const classes = useStyles();
+  return (
+    <div className={classes.root}>
+      <Grid container spacing={3}>
+        <Grid item xs={12}>
+          <Paper className={classes.paper}>Ratings and Reviews</Paper>
+        </Grid>
+        <Grid item xs={12} sm={4}>
+          <Paper className={classes.paper}>Ratings</Paper>
+        </Grid>
+        <Grid item xs={12} sm={8}>
+          <Paper className={classes.paper}>Reviews</Paper>
+        </Grid>
+      </Grid>
+    </div>
+  );
 };
 
 export default RatingsReviews;
