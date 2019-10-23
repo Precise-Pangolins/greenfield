8a44f14a2cda2cbcc2b270c8282a084398b994d1
Brittany Yee <brittanyyee1@gmail.com>
2019-10-23 16:40:57 -0400

    action creators to post new question and post new answer are working!

--- src/redux/actionCreators/QAActionCreators/createNewA.js
+++ src/redux/actionCreators/QAActionCreators/createNewA.js
@@ -5,7 +5,7 @@ const createNewAnswer = question_id => {
     return axios
       .post(`http://18.223.1.30/qa/${question_id}/answers`)
       .then(() => {
-        console.log("Successfully posted new answer");
+        dispatch({ type: "CREATE_NEW_ANSWER", answer: answer });
       })
       .catch(err => {
         console.log("Error in posting new answer");
