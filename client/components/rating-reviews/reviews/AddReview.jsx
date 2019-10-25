1f5ff1d6e97a31e268938e58a8d26e0470e97294
richard <richard.horvath001@gmail.com>
2019-10-24 20:06:02 -0400

    updated add reviews to include characteristics, summary, body, photos, name and email

--- client/components/rating-reviews/reviews/AddReview.jsx
+++ client/components/rating-reviews/reviews/AddReview.jsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState } from "react";
 import Button from "@material-ui/core/Button";
 import Dialog from "@material-ui/core/Dialog";
 import DialogActions from "@material-ui/core/DialogActions";
@@ -7,9 +7,21 @@ import DialogContentText from "@material-ui/core/DialogContentText";
 import DialogTitle from "@material-ui/core/DialogTitle";
 import useMediaQuery from "@material-ui/core/useMediaQuery";
 import { useTheme } from "@material-ui/core/styles";
-
+import Rating from "@material-ui/lab/Rating";
+import { makeStyles } from "@material-ui/core/styles";
+import Recommend from "./Recommend.jsx";
+import Characteristics from "./Characteristics.jsx";
+import TextField from "@material-ui/core/TextField";
+import CloudUploadIcon from "@material-ui/icons/CloudUpload";
+const useStyles = makeStyles(theme => ({
+  button: {
+    margin: theme.spacing(1)
+  }
+}));
 export default function ResponsiveDialog() {
-  const [open, setOpen] = React.useState(false);
+  const classes = useStyles();
+  const [open, setOpen] = useState(false);
+  const [rating, setRating] = useState(3);
   const theme = useTheme();
   const fullScreen = useMediaQuery(theme.breakpoints.down("sm"));
 
@@ -33,20 +45,76 @@ export default function ResponsiveDialog() {
         aria-labelledby="responsive-dialog-title"
       >
         <DialogTitle id="responsive-dialog-title">
-          {"Use Google's location service?"}
+          {"Write your Review"}
         </DialogTitle>
         <DialogContent>
-          <DialogContentText>
-            Let Google help apps determine location. This means sending
-            anonymous location data to Google, even when no apps are running.
-          </DialogContentText>
+          <DialogContentText>About the Product jabjbujg</DialogContentText>
+          <h3>Rating</h3>
+          <Rating
+            name="simple-controlled"
+            precision={0.25}
+            value={rating}
+            onChange={(event, newValue) => {
+              setRating(newValue);
+            }}
+          />
+          <Recommend />
+          <Characteristics />
+          <h3>Summary</h3>
+          <TextField
+            id="standard-full-width"
+            label=""
+            style={{ margin: 8 }}
+            placeholder="Ex: Best purchase ever!!"
+            fullWidth
+            margin="normal"
+            InputLabelProps={{
+              shrink: true
+            }}
+          />
+          <h3>Body</h3>
+          <TextField
+            id="standard-full-width"
+            label=""
+            style={{ margin: 8 }}
+            placeholder="Why did you like the product"
+            fullWidth
+            multiline
+            margin="normal"
+            InputLabelProps={{
+              shrink: true
+            }}
+          />
+          <h4>Add Photos</h4>
+          <Button
+            variant="contained"
+            color="default"
+            className={classes.button}
+            startIcon={<CloudUploadIcon />}
+          >
+            Upload
+          </Button>
+          <h3>Nick Name</h3>
+          <TextField
+            id="standard-name"
+            label="Name"
+            className={classes.textField}
+            margin="normal"
+          />
+          <h3>Email</h3>
+          <TextField
+            id="standard-name"
+            label="Name"
+            className={classes.textField}
+            margin="normal"
+          />
         </DialogContent>
         <DialogActions>
           <Button onClick={handleClose} color="primary">
-            Disagree
+            Exit
           </Button>
           <Button onClick={handleClose} color="primary" autoFocus>
-            Agree
+            Submit
           </Button>
         </DialogActions>
       </Dialog>
