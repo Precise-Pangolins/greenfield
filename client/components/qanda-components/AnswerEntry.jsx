4ec0e6c49cd0671ae4f83bc8aa60b4711ef98580
Brittany Yee <brittanyyee1@gmail.com>
2019-10-25 10:06:24 -0400

    Fixed typo in AnswerEntry component, able to render all questions within each question

--- client/components/qanda-components/AnswerEntry.jsx
+++ client/components/qanda-components/AnswerEntry.jsx
@@ -3,7 +3,7 @@ import React from "react";
 const AnswerEntry = answer => {
   console.log("answer in entry", answer);
 
-  return <div>A: P{answer.answer.body}</div>;
+  return <div>A: {answer.answer.body}</div>;
 };
 
 export default AnswerEntry;
