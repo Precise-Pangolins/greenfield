8f959fba7a24a7801e8118ad9a3f447e58190c73
Brittany Yee <brittanyyee1@gmail.com>
2019-10-26 10:57:26 -0400

    Search bar is functional, able to render only the searched questions out and otherwise will render default Qs if nothing is searched

--- client/components/qanda-components/QuestionsList.jsx
+++ client/components/qanda-components/QuestionsList.jsx
@@ -1,25 +1,56 @@
 import React, { useState, useEffect } from "react";
 import QuestionEntry from "./QuestionEntry.jsx";
 
-const QuestionsList = ({ questions, getAllQuestionsRequest, resultsQuestions, setResultQuestions }) => {
+const QuestionsList = ({
+  questions,
+  getAllQuestionsRequest,
+  resultsQuestions,
+  setResultQuestions
+}) => {
   const [page, setPage] = useState(2);
   useEffect(() => {
     getAllQuestionsRequest(1);
   }, []);
 
-
-  return (
-    <div>
-      {questions.map(question => {
-        return (
-          <div>
-            <QuestionEntry question={question} />
-          </div>
-        );
-      })}
-
-    </div>
-  );
+  if (resultsQuestions.length === 0) {
+    return (
+      <div>
+        {questions.map(question => {
+          return (
+            <div>
+              <QuestionEntry question={question} />
+            </div>
+          );
+        })}
+      </div>
+    );
+  } else {
+    return (
+      <div>
+        {resultsQuestions.map(resultsQuestion => {
+          {
+            console.log("resultsQuestion in MAP", resultsQuestion);
+          }
+          return (
+            <div>
+              <QuestionEntry question={resultsQuestion} />
+            </div>
+          );
+        })}
+      </div>
+    );
+  }
+  // return (
+  //   <div>
+  //       {questions.map(question => {
+  //         return (
+  //           <div>
+  //             <QuestionEntry question={question} />
+  //           </div>
+  //         )
+  //       })}
+  //   </div>
+  // );
 };
 
 export default QuestionsList;
