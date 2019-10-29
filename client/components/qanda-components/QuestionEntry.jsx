078f5d35f820bcc24cd4084116bed214716cd25e
Brittany Yee <brittanyyee1@gmail.com>
2019-10-29 10:18:12 -0400

    commiting to switch branches

--- client/components/qanda-components/QuestionEntry.jsx
+++ client/components/qanda-components/QuestionEntry.jsx
@@ -1,12 +1,12 @@
 import React from "react";
-import AnswerList from "./AnswersList.jsx";
+import AnswersList from "./AnswersList.jsx";
 
 const QuestionEntry = ({ question, answers }) => {
   return (
     <div class="questionEntry">
       {" "}
       <strong>Q:</strong> {question.question_body}
-      <AnswerList answers={answers} />
+      <AnswersList answers={answers} />
       <p>
         by {question.asker_name}, {question.question_date} | Helpful?{" "}
         <span>Yes</span> ({question.question_helpfulness}) | <span>Report</span>
