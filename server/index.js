bb7a1430dc221eae342b1d9f5f2d72436a2fd30d
Richard Horvath <richard.horvath001@gmail.com>
2019-11-01 18:01:48 -0400

    added gzip compression

--- server/index.js
+++ server/index.js
@@ -2,11 +2,13 @@ const express = require("express");
 const morgan = require("morgan");
 const Parser = require("body-parser");
 const app = express();
+const compression = require("compression ")
 const port = 3000;
 
 // app.get('/', (req, res) => res.send('Hello World!'));
 app.use(express.static("dist"));
 app.use(morgan("tiny"));
 app.use(Parser.json());
+app.use(compression());
 
 app.listen(port, () => console.log(`Example app listening on port ${port}!`));
