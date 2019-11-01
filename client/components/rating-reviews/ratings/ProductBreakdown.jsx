fcf9477658112e4f533090a16c2ebe9b30870503
richard <richard.horvath001@gmail.com>
2019-11-01 10:24:29 -0400

    fixed progress bars bug with dynamic info, added ticks to display the relevant ratings and updated the slider style

--- client/components/rating-reviews/ratings/ProductBreakdown.jsx
+++ client/components/rating-reviews/ratings/ProductBreakdown.jsx
@@ -2,22 +2,40 @@ import React from "react";
 import Grid from "@material-ui/core/Grid";
 
 const ProductBreakDown = ({ characteristics }) => {
+  console.log("CHARACTERISTIC", characteristics);
   return (
     <div className="product-breakdown-container">
       {Object.keys(characteristics).map(key => {
         return (
           <div>
             {key}{" "}
-            <div className>
-              <input
-                className="product-breakdown"
+            <div>
+              {/* <input
+                className="slider"
                 type="range"
                 name="points"
                 min="0"
                 max="5"
                 key={key}
-                value={characteristics[key]}
+                list="steplist"
+                value={characteristics[key].value}
+              ></input> */}
+              <input
+                type="range"
+                name="points"
+                min="0"
+                max="5"
+                value={characteristics[key].value}
+                className="slider"
               ></input>
+              <div class="sliderticks">
+                <p>0</p>
+                <p>1</p>
+                <p>2</p>
+                <p>3</p>
+                <p>4</p>
+                <p>5</p>
+              </div>
             </div>
           </div>
         );
