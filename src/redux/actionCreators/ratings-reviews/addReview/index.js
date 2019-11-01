8e608bca7010084ec5ffdf083471c3c7b8ae2cda
richard <richard.horvath001@gmail.com>
2019-11-01 09:34:32 -0400

    fixed add review bug

--- src/redux/actionCreators/ratings-reviews/addReview/index.js
+++ src/redux/actionCreators/ratings-reviews/addReview/index.js
@@ -2,13 +2,26 @@ const redux = require("redux");
 const axios = require("axios");
 const add = require("../../../actions/ReviewActions/addReview.js");
 const getReviews = require("../../../actions/ReviewActions/getReviews.js");
-
+const parseDate = require("../../../../utils/formatDate.js");
 const addReview = (id, review) => {
   return dispatch => {
     return axios
       .post(`http://18.223.1.30/reviews/${id}/`, review)
       .then(data => {
-        dispatch(add(review));
+        let obj = { ...review };
+        let photos = review.photos.map(url => {
+          return { id: 1, url };
+        });
+        let date = new Date();
+
+        obj.photos = photos;
+        obj.helpfullness = 0;
+        obj.reviewer_name = review.name;
+        obj.recommend = 0;
+        obj.response = "";
+
+        obj.date = parseDate(date.toString());
+        dispatch(add(obj));
       })
       .catch(error => console.error(error));
   };
