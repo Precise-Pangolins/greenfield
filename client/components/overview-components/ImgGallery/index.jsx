48fa8710c041cad10d9c47eb21e358f19558f4eb
Lauren Albert <pleaseonthekeys@gmail.com>
2019-12-29 14:31:55 -0500

    migrated image gallery zoom functionality from jQuery to React and fixed memory leaks

--- client/components/overview-components/ImgGallery/index.jsx
+++ client/components/overview-components/ImgGallery/index.jsx
@@ -1,76 +1,75 @@
-import React, { useEffect, useState, useRef } from 'react';
-import { connect } from 'react-redux';
-import queryString from 'querystring';
-import Carousel from 'react-image-gallery';
-import $ from 'jquery';
+import React, { useEffect, useState, useRef } from "react";
+import { connect } from "react-redux";
+import queryString from "querystring";
+import Carousel from "react-image-gallery";
+import $ from "jquery";
 
-import Thumbnails from './Thumbnails.jsx';
-import StyleSelector from '../StyleSelector/index.jsx';
+import Thumbnails from "./Thumbnails.jsx";
+import StyleSelector from "../StyleSelector/index.jsx";
 
-let productId = queryString.parse(location.search)['?productId'] || 1;
+let productId = queryString.parse(location.search)["?productId"] || 1;
+
+function onMouseOver(event) {
+  const img = event.target;
+  const topOffset = img.offsetTop;
+  const leftOffset = img.offsetLeft;
+  const height = img.height;
+  const width = img.width;
+
+  img.style.transformOrigin =
+    ((event.pageX - leftOffset) / width) * 50 +
+    "% " +
+    ((event.pageY - topOffset) / height) * 50 +
+    "%";
+}
 
 function ImgGallery({ currentStyle, info, images = [] }) {
   currentStyle = currentStyle || images[0] || [];
   let [isFullScreen, setIsFullScreen] = useState(false);
+  let [hasRegistered, setHasRegistered] = useState(false);
 
-  // let [initialImg, setInitialImg] = useState(
-  //   (images[0] ? images[0].photos : []).map(photo => {
-  //     console.log({ images });
-  //     return { original: photo.url, thumbnail: photo.thumbnail_url };
-  //   })
-  // );
-
-  let image = useRef(null); // updating  won't initiate a  re-render
-
-  // useEffect(() => {
-  //   setInitialImg();
-  //   return <Carousel items={initialImg} />;
-  // }, []);
+  let image = useRef(null);
 
   let modifiedImgs = (currentStyle.photos || initialImg || []).map(photo => {
     return { original: photo.url, thumbnail: photo.thumbnail_url };
   });
 
-  function onMouseOver(e) {
-    $('.tile').on('mousemove', function(e) {
-      $(this).css({
-        'transform-origin':
-          ((e.pageX - $(this).offset().left) / $(this).width()) * 50 +
-          '% ' +
-          ((e.pageY - $(this).offset().top) / $(this).height()) * 50 +
-          '%'
-      });
-    });
-  }
-
   function handleClick(event) {
-    event.persist();
-    if (isFullScreen) {
-      const img = event.target;
-      image.current = img;
-      img.className = 'tile';
-      if (img.style.transform === 'scale(2.5)') {
-        img.style.transform = 'scale(1)';
-        img.style.cursor = 'zoom-in';
-        return;
-      }
-      img.style.transform = 'scale(2.5)';
-      img.style.cursor = 'zoom-out';
-      onMouseOver(event);
+    if (!isFullScreen) {
+      return;
+    }
+    const img = event.target;
+    image.current = img;
+
+    img.className = "tile";
+    if (img.style.transform === "scale(2.5)") {
+      // here we are zooming out
+      img.style.transform = "scale(1)";
+      img.style.cursor = "zoom-in";
+      img.removeEventListener("mousemove", onMouseOver);
+      setHasRegistered(false);
+      return;
+    }
+    // here we are zooming in
+    img.style.transform = "scale(2.5)";
+    img.style.cursor = "zoom-out";
+    if (!hasRegistered) {
+      img.addEventListener("mousemove", onMouseOver);
+      setHasRegistered(true);
     }
   }
 
   function onScreenChange(fullScreenElem) {
     if (fullScreenElem) {
       setIsFullScreen(true);
-      image.current.style.cursor = 'zoom-in';
+      image.current.style.cursor = "zoom-in";
     } else {
       setIsFullScreen(false);
-      image.current.style.transform = 'scale(1)';
-      image.current.style.cursor = 'auto';
+      image.current.style.transform = "scale(1)";
+      image.current.style.cursor = "auto";
+      image.current.removeEventListener("mousemove", onMouseOver);
     }
   }
-
   return (
     <Carousel
       onScreenChange={onScreenChange}
